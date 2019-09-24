60e2af1fa365366432cf47dbde4ae4d482b28cf4
Deepansh Srivastava <deepansh2012@gmail.com>
2019-09-23 23:22:50 -0400

    update

--- test_file.py
+++ test_file.py
@@ -1,4 +1,4 @@
-from mrsimulator import Simulator, Isotopomer, Spectrum
+from mrsimulator import Simulator, Isotopomer, SpectroscopicDimension
 from mrsimulator.methods import one_d_spectrum
 import matplotlib.pyplot as plt
 
@@ -8,50 +8,51 @@ Isotopomers = [
             {
                 "isotope": "1H",
                 "isotropic_chemical_shift": "0 ppm",
-                "shielding_symmetric": {"anisotropy": "13.89 ppm", "asymmetry": 0.25},
+                "shielding_symmetric": {"zeta": "13.89 ppm", "eta": 0.25},
             }
         ],
         "abundance": "100 %",
     }
 ]
 
-spectrum = {
-    "direct_dimension": {
-        "magnetic_flux_density": "9.4 T",
-        "rotor_frequency": "0 kHz",
-        "rotor_angle": "54.735 deg",
-        "number_of_points": 2048,
-        "spectral_width": "25 kHz",
-        "reference_offset": "0 Hz",
-        "isotope": "1H",
-    }
+dimension = {
+    "magnetic_flux_density": "9.4 T",
+    "rotor_frequency": "0 kHz",
+    "rotor_angle": "54.735 deg",
+    "number_of_points": 2048,
+    "spectral_width": "25 kHz",
+    "reference_offset": "0 Hz",
+    "isotope": "1H",
 }
 
 sim = Simulator()
-sim.isotopomers = [Isotopomer.parse_json_with_units(item) for item in Isotopomers]
-sim.spectrum = Spectrum.parse_json_with_units(spectrum)
+sim.isotopomers = [Isotopomer.parse_dict_with_units(item) for item in Isotopomers]
+sim.spectrum = [SpectroscopicDimension.parse_dict_with_units(dimension)]
 
 freq1, amp1 = sim.run(one_d_spectrum, verbose=11)
 
 
-spectrum = {
-    "direct_dimension": {
-        "magnetic_flux_density": "9.4 T",
-        "rotor_frequency": "1 kHz",
-        "rotor_angle": "54.735 deg",
-        "number_of_points": 2048,
-        "spectral_width": "25 kHz",
-        "reference_offset": "0 Hz",
-        "isotope": "1H",
-    }
+dimension = {
+    "magnetic_flux_density": "9.4 T",
+    "rotor_frequency": "1 kHz",
+    "rotor_angle": "54.735 deg",
+    "number_of_points": 2048,
+    "spectral_width": "25 kHz",
+    "reference_offset": "0 Hz",
+    "isotope": "1H",
 }
 
-sim.spectrum = Spectrum.parse_json_with_units(spectrum)
+sim.spectrum = [SpectroscopicDimension.parse_dict_with_units(dimension)]
 freq2, amp2 = sim.run(one_d_spectrum, verbose=11)
 
-fig, ax = plt.subplots(1, 2)
-ax[0].plot(freq1, amp1)
-ax[0].set_xlabel("frequency / Hz")
-ax[1].plot(freq2, amp2)
-ax[1].set_xlabel("frequency / Hz")
+fig, ax = plt.subplots(1, 2, figsize=(6, 3))
+ax[0].plot(freq1, amp1, linewidth=1.0, color="k")
+ax[0].set_xlabel(f"frequency ratio / {freq2.unit}")
+ax[0].grid(color="gray", linestyle="--", linewidth=0.5, alpha=0.5)
+ax[0].set_title("Static")
+ax[1].plot(freq2, amp2, linewidth=1.0, color="k")
+ax[1].set_xlabel(f"frequency ratio / {freq2.unit}")
+ax[1].grid(color="gray", linestyle="--", linewidth=0.5, alpha=0.5)
+ax[1].set_title("MAS")
+plt.tight_layout(h_pad=0, w_pad=0, pad=0)
 plt.show()
