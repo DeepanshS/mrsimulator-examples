3b4669eaa6a5ce6c2d23bb0305a2a9da95dd461b
Deepansh Srivastava <deepansh2012@gmail.com>
2019-11-19 12:24:13 -0500

    Update the example to latest mrsimulator version.

--- test_file.py
+++ test_file.py
@@ -1,9 +1,8 @@
-from mrsimulator import Simulator, Isotopomer, SpectroscopicDimension
+from mrsimulator import Simulator, Isotopomer, Dimension
 from mrsimulator.methods import one_d_spectrum
 import matplotlib.pyplot as plt
 
-# setting isotopomers
-Isotopomers = [
+isotopomers = [
     {
         "sites": [
             {
@@ -16,7 +15,6 @@ Isotopomers = [
     }
 ]
 
-# static spectrum setup
 dimension = {
     "magnetic_flux_density": "9.4 T",
     "rotor_frequency": "0 kHz",
@@ -29,25 +27,15 @@ dimension = {
 
 sim = Simulator()
 sim.isotopomers = [Isotopomer.parse_dict_with_units(item) for item in Isotopomers]
-sim.spectrum = [SpectroscopicDimension.parse_dict_with_units(dimension)]
+sim.dimensions = [Dimension.parse_dict_with_units(dimension)]
 
-freq1, amp1 = sim.run(one_d_spectrum, verbose=11)
+freq1, amp1 = sim.run(one_d_spectrum)
 
-# magic angle spinning (MAS) spectrum setup
-dimension = {
-    "magnetic_flux_density": "9.4 T",
-    "rotor_frequency": "1 kHz",
-    "rotor_angle": "54.735 deg",
-    "number_of_points": 2048,
-    "spectral_width": "25 kHz",
-    "reference_offset": "0 Hz",
-    "isotope": "1H",
-}
+# spinning sideband simulation
+# set the rotor frequency to 1000 Hz
+sim.dimensions[0].rotor_frequency = 1000
+freq2, amp2 = sim.run(one_d_spectrum)
 
-sim.spectrum = [SpectroscopicDimension.parse_dict_with_units(dimension)]
-freq2, amp2 = sim.run(one_d_spectrum, verbose=11)
-
-# plot of the two spectrum
 fig, ax = plt.subplots(1, 2, figsize=(6, 3))
 ax[0].plot(freq1, amp1, linewidth=1.0, color="k")
 ax[0].set_xlabel(f"frequency ratio / {freq2.unit}")
